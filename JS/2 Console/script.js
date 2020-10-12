89590e364830f8fdb2a24fb428903fd2acad87ee
Isedor021 <joakim.wahlstrom@lexicon.se>
2020-10-12 14:10:02 +0200

    lektion 5

---
+++ JS/2 Console/script.js
@@ -0,0 +1,14 @@
+// en kommentar kan skrivas såhär
+
+/*
+    en kommentar kan skrivas såhär
+    om det är flera rader.
+*/
+
+// Skriver ut på själva hemsidan
+document.body.innerText = 'Detta är en text';
+
+// Skriver ut i konsolen
+console.log('Här skiver vi ut någonting i konsolen');
+console.warn('Detta är en varning');
+console.error('Detta är ett felmeddelande');
